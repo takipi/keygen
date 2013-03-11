879dcb956609be0d8173c98539e5c1199b833aa0
Chen Harel <chook.harel@gmail.com>
2013-03-11 10:39:34 +0200

    Some changes

--- src/com/takipi/keygen/SecretKeyGenerator.java
+++ src/com/takipi/keygen/SecretKeyGenerator.java
@@ -4,14 +4,16 @@ public class SecretKeyGenerator
 {
 	private static final int CHECKSUM_SIZE = 4;
 	
+	private static final String KEY_DELIM = "#";
+	
 	public static String generateKey(String prefix)
 	{
 		String privateKey = KeyUtil.getSecureRandomString();
 		
-		String combineKey = prefix + "#" + privateKey;
+		String combineKey = prefix + KEY_DELIM + privateKey;
 		
 		String checksum = KeyUtil.generateMd5ForKey(combineKey, CHECKSUM_SIZE);
 		
-		return combineKey + "#" + checksum;
+		return combineKey + KEY_DELIM + checksum;
 	}
 }
