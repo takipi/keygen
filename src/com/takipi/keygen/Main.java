78a98c3342bdb72a2d4ce562d62facb8264964a3
Chen Harel <chook.harel@gmail.com>
2013-03-11 15:05:05 +0200

    Source refactoring

--- src/com/takipi/keygen/Main.java
+++ src/com/takipi/keygen/Main.java
@@ -1,32 +1,47 @@
 package com.takipi.keygen;
 
+import com.takipi.keygen.instructions.InstructionsBuilder;
+import com.takipi.keygen.key.SecretKeyGenerator;
+import com.takipi.keygen.net.TakipiCom;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		if (args.length != 3)
+		if (!(args.length == 3 || args.length == 4))
 		{
-			System.out.println("Takipi key Generator");
-			System.out.println("Usage: USERNAME PASSWORD NAMESPACES");
+			System.out.println("Takipi Key Generator");
+			System.out.println("====================");
+			System.out.println("Usage: USERNAME PASSWORD NAMESPACES [SERVER-PROXY]");
 			System.out.println("Example: john@example.com Pa$$woRd com.company;org.company2.product");
 			
 			return;
 		}
 		
-		String username = args[0];
-		String password = args[1];
-		String namespaces = args[2];
+		String username 	= args[0];
+		String password 	= args[1];
+		String namespaces 	= args[2];
 		
-		String keyPrefix = NetUtil.generateKeyPrefix(username, password, namespaces);
+		String proxy = "";
+		
+		if (args.length == 4)
+		{
+			proxy = args[3];
+		}
+		
+		String keyPrefix = TakipiCom.generateKeyPrefix(username, password, namespaces);
 		
 		if (keyPrefix == null)
 		{
 			System.err.println("Problem creating key");
+			System.err.println("Contact keygen@takipi.com for help.");
 			return;
 		}
 		
 		String secretKey = SecretKeyGenerator.generateKey(keyPrefix); 
 		
 		System.out.println("Your secret key is: " + secretKey);
+		
+		InstructionsBuilder.buildInstructionsFile(username, secretKey, proxy);
 	}
 }
--- src/com/takipi/keygen/util/HexUtil.java
+++ src/com/takipi/keygen/util/HexUtil.java
@@ -31,5 +31,4 @@ public class HexUtil
 			throw new IllegalStateException(e);
 		}
 	}
-	
 }
